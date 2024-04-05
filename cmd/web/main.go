52e5372f65c94cd2339ea29adefab139f24c3b9a
oyevamos <deadhead2520@gmail.com>
2024-04-05 11:18:03 +0300

    commit

---
+++ cmd/web/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+)
+
+func main() {
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	flag.Parse()
+
+	mux := http.NewServeMux()
+
+	fileServer := http.FileServer(http.Dir("./ui/static"))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
+
+	log.Printf("Starting server on %s", *addr)
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
+}
