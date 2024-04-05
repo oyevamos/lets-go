b6942e2b44ef903486e752492737f40c95fda9aa
oyevamos <deadhead2520@gmail.com>
2024-04-05 23:53:44 +0300

    commit

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -2,7 +2,36 @@ package main
 
 import (
 	"github.com/oyevamos/lets-go.git/internal/models"
+	"html/template"
+	"path/filepath"
 )
+
+func newTemplateCache() (map[string]*template.Template, error) {
+	cache := map[string]*template.Template{}
+
+	pages, err := filepath.Glob("ui/html/pages/*.tmpl")
+	if err != nil {
+		return nil, err
+	}
+	for _, page := range pages {
+
+		name := filepath.Base(page)
+
+		files := []string{
+			"./ui/html/base.tmpl",
+			"./ui/html/partials/nav.tmpl",
+			page,
+		}
+
+		ts, err := template.ParseFiles(files...)
+		if err != nil {
+			return nil, err
+		}
+
+		cache[name] = ts
+	}
+	return cache, nil
+}
 
 type templateData struct {
 	Snippet  *models.Snippet
